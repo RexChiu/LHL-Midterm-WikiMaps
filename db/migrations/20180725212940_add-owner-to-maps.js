c855385d94729f0cf73930f63d358b42ab73321a
Your Name <alex.zvaniga@gmail.com>
2018-07-25 17:34:34 -0400

    git

--- db/migrations/20180725212940_add-owner-to-maps.js
+++ db/migrations/20180725212940_add-owner-to-maps.js
@@ -1,18 +1,19 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table("maps", function(table) {
+    knex.schema.table('maps', function(table) {
+      table.integer('user_id');
       table
-        .foreign("user_id")
-        .references("id")
-        .inTable("users");
+        .foreign('user_id')
+        .references('id')
+        .inTable('users');
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table("maps", function(table) {
-      table.dropColumn("user_id");
+    knex.schema.table('maps', function(table) {
+      table.dropColumn('user_id');
     })
   ]);
 };
