bbbc03febc88da479048da2cc98ef3ecfde5b9fa
Your Name <alex.zvaniga@gmail.com>
2018-07-26 18:28:27 -0400

    added script with html

---
+++ public/scripts/pub-maps.js
@@ -0,0 +1,19 @@
+$(() => {
+  var html = `<h2>Public Maps</h2>
+  <div class="mapTitle">TITLE</div>
+  <div class="map">MAP</div>
+  <div class="map_desc">
+    <textarea rows="4" cols="50">
+    This is where the map description will go - The map description is 4 rows by 50 columns.
+    </textarea>
+  </div>
+  <div>points
+    <ul>
+      <li>point 1</li>
+      <li>point 1</li>
+      <li>point 1</li>
+      <li>point 1</li>
+      <li>point 1</li>
+    </ul>
+  </div>`;
+});
