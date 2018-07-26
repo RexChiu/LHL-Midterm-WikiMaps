fe1116ec73a06d7a9e71d162fd6d17cf3ac661d6
Rex Chiu <rex.chiu@outlook.com>
2018-07-26 14:12:10 -0400

    added server side code handling for POST /maps

--- routes/maps.js
+++ routes/maps.js
@@ -4,6 +4,8 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
+const randomString = require('random-string');
+const dataHelpers = require('../');
 
 const API_KEY = process.env.API_KEY;
 
@@ -18,6 +20,58 @@ module.exports = knex => {
       API_KEY: API_KEY
     };
     res.render('new-map', templateVars);
+  });
+
+  //User can create a map with a map type - and a static map image
+  // POST /maps
+  router.post('/', (req, res) => {
+    let name = req.body.name;
+    let username = 't'; //temp hardcoded username
+    let url =
+      'http://localhost:8080/maps/' +
+      randomString({
+        length: 5,
+        numeric: true,
+        letters: true
+      });
+    let desc = req.body.desc;
+    let visible = req.body.public;
+    let rating = null;
+    let type = req.body.type;
+    let start_lat = req.body.lat;
+    let start_lng = req.body.lng;
+    let img_url = 'https://i.pinimg.com/originals/78/5c/39/785c39aa38a5867388fe432079f7808d.jpg';
+
+    //find user_id
+    knex
+      .select('id')
+      .from('users')
+      .where('username', 't')
+      .then(id => {
+        //insert map once user_id is found
+        knex
+          .insert({
+            name: name,
+            user_id: id,
+            url: url,
+            desc: desc,
+            public: visible,
+            rating: rating,
+            start_lat: start_lat,
+            start_lng: start_lng,
+            img_url: img_url
+          })
+          .into('maps')
+          .then(result => {
+            res.send(url);
+          })
+          .catch(err => {
+            res.status(500).send(err);
+          });
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
   });
 
   //End of routes
