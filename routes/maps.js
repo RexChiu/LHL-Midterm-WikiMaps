eb8bc18da065bde975a05d0978cad41457cae07b
Rex Chiu <rex.chiu@outlook.com>
2018-07-26 12:36:41 -0400

    added new-maps placeholder page

--- routes/maps.js
+++ routes/maps.js
@@ -3,11 +3,22 @@
 
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
 
 module.exports = knex => {
   //User can see maps available to them with static images
   //GET /maps
   router.get('/', (req, res) => {});
+
+  //User can see a page to create a new map
+  router.get('/new', (req, res) => {
+    let templateVars = {
+      API_KEY: API_KEY
+    };
+    res.render('new-map', templateVars);
+  });
 
   //End of routes
   return router;
