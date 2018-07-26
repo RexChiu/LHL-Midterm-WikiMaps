2c81a6c03faeec36c31ccf05a30b4202bcf2d1bc
Your Name <alex.zvaniga@gmail.com>
2018-07-26 12:58:35 -0400

    changed default map to Toronto coordinates

--- public/scripts/new-map.js
+++ public/scripts/new-map.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 43.653, lng: -79.383 },
     zoom: 8
   });
 }
