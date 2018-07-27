ba2d39c19ed8442a84b27ab2bc35bb865a6aecc0
Rex Chiu <rex.chiu@outlook.com>
2018-07-27 19:30:24 -0400

    added points functionality

--- public/scripts/map-details.js
+++ public/scripts/map-details.js
@@ -3,8 +3,47 @@ $(() => {
   var lng = $('#map').data().lng;
 
   map.setCenter({ lat: lat, lng: lng });
+
+  $('.send-points-btn').on('click', ev => {
+    ev.preventDefault(); //prevent default event from occuring  })
+    ev.stopImmediatePropagation();
+
+    console.log(mapMarkers);
+
+    var markers = [];
+    var mapId = $('.map-url')
+      .attr('href')
+      .split('/maps/')[1];
+
+    for (var i = 0; i < mapMarkers.length; i++) {
+      var markerObj = {
+        mapId: mapId,
+        lat: mapMarkers[i].getPosition().lat(),
+        lng: mapMarkers[i].getPosition().lng(),
+        addr: mapAddresses[i]
+      };
+      markers.push(markerObj);
+    }
+
+    console.log(markers);
+
+    var payload = {
+      markers: markers
+    };
+
+    console.log('payload' + payload);
+    console.log('mapId ' + mapId);
+
+    $.post(`/maps/${mapId}/points`, payload)
+      .done(resp => {
+        console.log(resp);
+      })
+      .fail(err => console.log(err.message));
+  });
 });
 
+var mapMarkers = [];
+var mapAddresses = [];
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -16,5 +55,32 @@ function initMap() {
     var latitude = event.latLng.lat();
     var longitude = event.latLng.lng();
     console.log(latitude + ', ' + longitude);
+
+    var marker = new google.maps.Marker({
+      position: {
+        lat: latitude,
+        lng: longitude
+      },
+      map: map
+    });
+    mapMarkers.push(marker);
+
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode(
+      {
+        location: {
+          lat: latitude,
+          lng: longitude
+        }
+      },
+      function(results, status) {
+        if (status === 'OK') {
+          if (results[0]) {
+            console.log(results[0].formatted_address);
+            mapAddresses.push(results[0].formatted_address);
+          }
+        }
+      }
+    );
   });
 }
--- routes/points.js
+++ routes/points.js
@@ -3,11 +3,87 @@
 
 const express = require('express');
 const router = express.Router();
+const randomString = require('random-string');
 
 module.exports = knex => {
   //User can see many points on a map
   //GET /maps/:id/points
   router.get('/', (req, res) => {});
+
+  //User can add a map points
+  //POST /maps/:id/points
+  router.post('/', (req, res) => {
+    let url =
+      'http://localhost:8080/points/' +
+      randomString({
+        length: 5,
+        numeric: true,
+        letters: true
+      });
+    insertPoints(res, req.body.markers, url);
+
+    res.send('Cats.');
+  });
+
+  //Data Helper Functions
+
+  function insertPoints(res, points, url) {
+    //grabs the points, and calls insertPoint for every point
+
+    for (let elem of points) {
+      insertPoint(res, elem).catch(err => {
+        console.log(err);
+        res.status(500).send(err);
+        return;
+      });
+    }
+  }
+
+  function insertPoint(res, point, url) {
+    //get the ID of the mapId
+    return getMapId(point.mapId).then(mapId => {
+      let data = {
+        map_id: mapId,
+        title: null,
+        desc: null,
+        img_url: null,
+        rating: null,
+        lat: point.lat,
+        lng: point.lng,
+        addr: point.addr,
+        url: url
+      };
+
+      // insert map once user_id is found
+      knex('points')
+        .insert(data)
+        .then(result => {
+          console.log(result);
+          return Promise.resolve(result);
+        })
+        .catch(err => {
+          return Promise.reject(err);
+        });
+
+      return Promise.resolve(point);
+    });
+  }
+
+  function getMapId(url) {
+    // find map by URL
+    console.log('url: ' + url);
+
+    return knex
+      .select('id')
+      .from('maps')
+      .where('url', 'ilike', `%${url}%`)
+      .then(result => {
+        return Promise.resolve(result[0].id);
+      })
+      .catch(err => {
+        return Promise.reject(err);
+      });
+  }
 
   //End of routes
   return router;
