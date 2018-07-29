7db2db85f129e621fe58724e658271727574be07
Rex Chiu <rex.chiu@outlook.com>
2018-07-29 11:43:52 -0400

    moved scss files into main.scss

--- public/scripts/user-maps.js
+++ public/scripts/user-maps.js
@@ -22,17 +22,28 @@ $(() => {
     }
   });
   //favourite maps
-  // $.getJSON('/users/${userid}/fav').done(maps => {
-  //   for (let map of maps) {
-  //     var html = `
-  // <div class="mapTitle"><a href="${map.url}">${map.name}</a></div>
-  // <div class="map"><img src="${map.img_url}" alt="Google Map Static Image"></div>
-  // <div class="map_desc">
-  //   <textarea rows="4" cols="50">
-  //   ${map.desc}
-  //   </textarea>
-  // </div>
-  // <div class="rating">${map.rating}
+  //get the maps that belong to the user
+  $.getJSON(`/users/fav`).done(maps => {
+    for (let map of maps) {
+      //grabs the map url/id
+      var mapId = map.url.split('/maps/')[1];
+      var html = `
+  <div class="mapTitle"><a href="${map.url}">${map.name}</a></div>
+  <div class="map"><img src="${map.img_url}" alt="Google Map Static Image"></div>
+  <div class="map_desc">
+    <textarea rows="4" cols="50">
+    ${map.desc}
+    </textarea>
+  </div>
+  <div class="rating">${map.rating}
+  
+  </div>
+  <div>points
+    
+  </div>`;
+      $('.favMapContainer').prepend(html);
+    }
+  });
 
   // </div>
   // <div>points
