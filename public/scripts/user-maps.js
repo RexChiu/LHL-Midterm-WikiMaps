b2248e54170f73d15180f5c7802cf46b6357620b
Alexander Zvaniga <alex.zvaniga@gmail.com>
2018-07-29 02:44:57 +0000

    removed commented html

--- public/scripts/user-maps.js
+++ public/scripts/user-maps.js
@@ -36,29 +36,4 @@ $(() => {
 
   // </div>
   // <div>points
-
-  // </div>`;
-  //     $('.yourMapContainer').prepend(html);
-  //   }
-  // });
-  //
-  //   $.getJSON('/users/${userid}/contributor').done(maps => {
-  //     for (let map of maps) {
-  //       var html = `
-  //   <div class="mapTitle"><a href="${map.url}">${map.name}</a></div>
-  //   <div class="map"><img src="${map.img_url}" alt="Google Map Static Image"></div>
-  //   <div class="map_desc">
-  //     <textarea rows="4" cols="50">
-  //     ${map.desc}
-  //     </textarea>
-  //   </div>
-  //   <div class="rating">${map.rating}
-
-  //   </div>
-  //   <div>points
-
-  //   </div>`;
-  //       $('.yourMapContainer').prepend(html);
-  //     }
-  //   });
 });
