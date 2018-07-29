ad9703cd987414431d5e17f532e694ff2161f355
Your Name <alex.zvaniga@gmail.com>
2018-07-29 19:30:41 -0400

    updated style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,10 +13,18 @@ $(() => {
       username: $('#login-username').val(),
       password: $('#login-password').val()
     };
-    $.post('/users/login', requestPayload).then(response => {
-      console.log('This is the response', response);
-      window.location.href = 'http://localhost:8080/profile';
-    });
+    if (!requestPayload.username || !requestPayload.password) {
+      alert('Please enter both a Username and a Password');
+    } else {
+      $.post('/users/login', requestPayload)
+        .done(response => {
+          console.log('This is the response', response);
+          window.location.href = 'http://localhost:8080/profile';
+        })
+        .fail(err => {
+          alert(err);
+        });
+    }
   });
   // REGISTER FORM BUTTON EVENT LISTENER
   $('#register-form-btn').on('click', ev => {
@@ -28,15 +36,38 @@ $(() => {
       password: $('#register-password').val(),
       password_confirm: $('#register-password_confirm').val()
     };
-    $.post('/users/register', requestPayload).then(response => {
-      window.location.href = 'http://localhost:8080/profile';
-    });
+    $.post('/users/register', requestPayload)
+      .done(response => {
+        window.location.href = 'http://localhost:8080/profile';
+      })
+      .fail(err => {
+        alert(err);
+      });
   });
   // LOGOUT BUTTON EVENT LISTENER
   $('#logout-btn').on('click', ev => {
     ev.preventDefault();
-    $.post('/users/logout').then(res => {
-      window.location.href = 'http://localhost:8080/';
-    });
+    $.post('/users/logout')
+      .done(res => {
+        window.location.href = 'http://localhost:8080/';
+      })
+      .fail(err => {
+        alert(err);
+      });
+  });
+  // HOME BUTTON EVENT LISTENER
+  $('#home-btn').on('click', ev => {
+    ev.preventDefault();
+    window.location.href = 'http://localhost:8080/';
+  });
+  // PROFILE BUTTON EVENT LISTENER
+  $('#logout-btn').on('click', ev => {
+    ev.preventDefault();
+    window.location.href = 'http://localhost:8080/profile';
+  });
+  // NEW MAP BUTTON EVENT LISTENER
+  $('#new-map-btn').on('click', ev => {
+    ev.preventDefault();
+    window.location.href = 'http://localhost:8080/maps/new';
   });
 });
