650867c826023ed1d43425f074d3315d45317af2
Rex Chiu <rex.chiu@outlook.com>
2018-07-30 10:12:01 -0400

    changed knex configs for prod

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 
 module.exports = {
-
   development: {
     client: 'postgresql',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: process.env.DB_SSL
     },
     migrations: {
       directory: './db/migrations',
@@ -23,14 +22,18 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection:
+      'postgres://kwgkimihhfxjnp:97ba9fadbdb9b9c4617f32840cb0e8f6ca2d54ec6ec56a9dd7bf67e90e36abd9@ec2-107-22-192-11.compute-1.amazonaws.com:5432/dqvqb0jq09ooh',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
-
 };
