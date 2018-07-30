777c3796a5d73878cbe6520bb819768e5dd84e5c
Rex Chiu <rex.chiu@outlook.com>
2018-07-30 09:54:55 -0400

    fixed even more HTML stylings

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -50,26 +50,26 @@ $(() => {
         //grab the points
         $.get(`/maps/${mapId}/points`).done(points => {
           html = `<div class="row featurette border border-dark py-2 mt-4">
-      <div class="col-md-6 push-md-6">
-        <h2 class="featurette-heading text-center"><a href="${map.url}">${map.name}</a></h2>
-        <p class="lead text-center">${map.desc}</p>
-      `;
+        <div class="col-md-6 push-md-6">
+          <h2 class="featurette-heading text-center"><a href="${map.url}">${map.name}</a></h2>
+          <p class="lead text-center">${map.desc}</p>
+        `;
           for (var i = 0; i < points.length; i++) {
             html += `<div><ul>
-        <li>Title: ${points[i].title}</li>
-        <li>Desc: ${points[i].desc}</li>
-        <li>Address: ${points[i].addr}</li>
-        <!--<li>URL: ${points[i].url}</li>-->
-        </ul></div>`;
+          <li>Title: ${points[i].title}</li>
+          <li>Desc: ${points[i].desc}</li>
+          <li>Address: ${points[i].addr}</li>
+          <!--<li>URL: ${points[i].url}</li>-->
+          </ul></div>`;
           }
           html += `</div>
-      <div class="col-md-6 pull-md-6">
-        <img class="featurette-image img-fluid mx-auto border border-dark" data-src="holder.js/500x500/auto" alt="500x500" src="${
-          map.img_url
-        }" data-holder-rendered="true"
-          style="width: 500px; height: 500px;">
-      </div>
-    <hr class='featurette-divider">`;
+        <div class="col-md-6 pull-md-6">
+          <img class="featurette-image img-fluid mx-auto border border-dark rounded" data-src="holder.js/500x500/auto" alt="500x500" src="${
+            map.img_url
+          }" data-holder-rendered="true"
+            style="width: 500px; height: 500px;">
+        </div>
+      <hr class='featurette-divider"/>`;
           $('.favMapContainer').append(html);
         });
       }
